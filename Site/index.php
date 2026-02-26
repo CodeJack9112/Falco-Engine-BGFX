8279dc087e3117f535ba0baa10257be8feb8ac74
CodeJack9112 <166967144+CodeJack9112@users.noreply.github.com>
2026-02-26 16:43:27 +0600

    Commit Site

---
+++ Site/index.php
@@ -0,0 +1,9 @@
+<html>
+How to be involved in our Great Engine development and community:<br>
+1. Send your work email, login, password to falcoware[at]gmail.com.<br>
+2. Please update your reposition:<br>
+svn://svn.falco3d.com/<br>
+<br>
+Good luck!<br>
+
+</html>
